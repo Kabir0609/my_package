3bd048b7fd8a05b40b33721cb7d25f2d0efa779f
Kabir0609 <kcanarte@espol.edu.ec>
2023-10-26 23:57:11 -0500

    First Commit

--- launch/listener.launch.py
+++ launch/listener.launch.py
@@ -1,10 +1,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_descrption():
-    return LaunchDescription([ 
+def generate_launch_description():
+    return LaunchDescription([
         Node(
-            package='demo_nodes_cpp',
+            package='demo_nodes_py',
             executable='listener'
         )
     ])
